1cd93748fef519a0e7ac3a2a31bd2948d6b7f2a4
yehorkhod <e.khodysko@gmail.com>
2025-02-07 19:12:41 +0200

    feat: add db

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,12 +1,41 @@
+import os
 from flask import Flask
 from flask_sock import Sock
+from flask_sqlalchemy import SQLAlchemy
+import dotenv
 
+# Load environment variables
+dotenv.load_dotenv()
+
+# Define the configuration class
+class Config:
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://"
+        f"{os.getenv('POSTGRE_USER')}"
+        ":"
+        f"{os.getenv('POSTGRE_PASSWORD')}"
+        "@localhost/"
+        f"{os.getenv('POSTGRE_DB')}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# API setup
 app: Flask = Flask(__name__)
-sock: Sock = Sock(app)
+app.config.from_object(Config)
 
+sock: Sock = Sock(app)
+db: SQLAlchemy = SQLAlchemy(app)
+
+# Routes
+@app.route('/home')
+def home():
+    return 'Hi mom!'
 
 @sock.route('/echo')
 def echo(ws):
     while True:
         data = ws.receive()
         ws.send(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
